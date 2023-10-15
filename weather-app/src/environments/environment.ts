d11725246b65535c64553585bbc5a2001b4be9d9
Roshan Singh <singh.roshan9718@gmail.com>
2023-10-15 20:48:03 +0530

    set env and services

---
+++ weather-app/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city/landon',
+    XRapidAPIHostHeaderName:'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+    XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue:'7539888059msh0209bba93355f44p118610jsn30366447e740'
+};
